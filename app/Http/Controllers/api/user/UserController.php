1900a8b5417a84d511df482df683940232adaada
imily <uiyt48@livemail.tw>
2019-06-29 13:14:36 +0800

    修改 userController

--- app/Http/Controllers/api/user/UserController.php
+++ app/Http/Controllers/api/user/UserController.php
@@ -3,7 +3,6 @@
 use App\Classes\Common\HttpStatusCode;
 use App\Classes\Errors\ErrorArgument;
 use App\Classes\Errors\ErrorAuth;
-use App\Classes\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
 use App\Models\UserModel;
@@ -11,55 +10,6 @@ use Illuminate\Support\Facades\Input;
 
 class UserController extends Controller
 {
-    /**
-     * 使用者註冊
-     * URI:PATCH /api/registered
-     * @return Response
-     */
-    public function userRegistered()
-    {
-        $statusCode = HttpStatusCode::STATUS_201_CREATED;
-        $response = array();
-
-        $hasStickerType = Input::has('sticker_type');
-        $hasAccount = Input::has('username');
-        $hasPassword = Input::has('password');
-
-        if (( ! $hasStickerType) or
-            ( ! $hasAccount) or
-            ( ! $hasPassword)) {
-            $error = new ErrorArgument(ErrorArgument::ERROR_ARGUMENT_EMPTY_INPUT);
-            $statusCode = HttpStatusCode::STATUS_400_BAD_REQUEST;
-            return response()->json($error->convertToDisplayArray(), $statusCode);
-        }
-
-        $stickerType = (String)Input::get('sticker_type');
-        $account = (String)Input::get('username');
-        $password = (String)Input::get('password');
-
-        if (($stickerType == '') or
-            ($account == '') or
-            ($password == '')) {
-            $error = new ErrorArgument(ErrorArgument::ERROR_ARGUMENT_EMPTY_INPUT);
-            $statusCode = HttpStatusCode::STATUS_400_BAD_REQUEST;
-            return response()->json($error->convertToDisplayArray(), $statusCode);
-        }
-
-        $user = new User();
-        $user->setStickerType($stickerType);
-        $user->setUsername($account);
-
-        list($isSuccess, $error) = UserModel::registerUser($user, $password);
-
-        if (! $isSuccess) {
-            $statusCode = HttpStatusCode::STATUS_400_BAD_REQUEST;
-            return response()->json($error->convertToDisplayArray(), $statusCode);
-        }
-
-        $response['id'] = $user->getId();
-        return response()->json($response, $statusCode);
-    }
-
     /**
      * 取得目前登入的使用者
      * URI: GET /api/user/info
@@ -109,7 +59,7 @@ class UserController extends Controller
             return response()->json($error->convertToDisplayArray(), $statusCode);
         }
 
-        if(($newPassword) != ($confirmPassword)){
+        if (($newPassword) != ($confirmPassword)) {
             $statusCode = HttpStatusCode::STATUS_400_BAD_REQUEST;
             $error = new ErrorAuth(ErrorAuth::ERROR_AUTH_UNMATCHED_PASSWORD);
             return response()->json($error->convertToDisplayArray(), $statusCode);
