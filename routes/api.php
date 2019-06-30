9d62e1f507d33e3b4fc5c70a10629d1d3197b39f
imily <uiyt48@livemail.tw>
2019-06-30 13:20:30 +0800

    新增 api 的路徑

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['namespace' => 'api\user'], function () {
-    //以名稱取得資料的API
+Route::group(['namespace' => 'api'], function () {
+    // 取得討論主題部分資料
     Route::get('posts', 'PostController@getPost');
+});
+
+Route::group(['namespace' => 'api\user'], function () {
+    // 以userId取得部分討論主題
+    Route::get('users/{userId}/posts', 'PostController@getPostByUserId');
+    // 以postId取得單一討論主題
+    Route::get('posts/{postId}', 'PostController@getPostById');
+    // 新增單一討論主題
+    Route::post('posts', 'PostController@addPost');
+    // 修改單一討論主題
+    // 批量刪除討論主題
 });
