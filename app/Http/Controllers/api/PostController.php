b904ac195dca8a31e020e98914155027076ac731
imily <uiyt48@livemail.tw>
2019-06-30 13:20:14 +0800

    新增無須登入的 postController + test

---
+++ app/Http/Controllers/api/PostController.php
@@ -0,0 +1,115 @@
+<?php namespace App\Http\Controllers\api;
+
+use App\Classes\Common\HttpStatusCode;
+use App\Classes\Errors\ErrorArgument;
+use App\Http\Controllers\Controller;
+use App\Models\PostModel;
+use App\Repositories\Filter;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Input;
+use App\Classes\Post;
+
+class PostController extends Controller
+{
+    /**
+     * 將Posts轉換成要顯示的陣列內容
+     * @param Post[] $posts
+     * @return array
+     */
+    private function convertToPostsDisplayArray(array $posts)
+    {
+        $response = array();
+        foreach ($posts as $post) {
+            $messages = $post->getMessage();
+            $users = $post->getUser();
+            $postUser = $post->getUserObject();
+
+            $postContent = array();
+            $postContent['id'] = $post->getId();
+            $postContent['user_name'] = $postUser->getUsername();
+            $postContent['user_sicker_type'] = $postUser->getStickerType();
+            $postContent['messages']['total_amount'] = count($messages);
+
+            $messageContents = array();
+            $messageContent = array();
+            foreach ($messages as $message) {
+                $messageUser = $message->getUser();
+                $messageContent['user_name'] = $messageUser->getUsername();
+                $messageContent['description'] = $message->getDescription();
+                $messageContents[] = $messageContent;
+            }
+
+            $postContent['messages']['data'] = $messageContents;
+            $postContent['topic'] = $post->getTopic();
+            $postContent['description'] = $post->getDescription();
+
+            $likeContents = array();
+            $likeContent = array();
+            foreach ($users as $user) {
+                $likeContent['user_name'] = $user->getUsername();
+                $likeContents[] = $likeContent;
+            }
+
+            $postContent['likes'] = $likeContents;
+            $postContent['create_time'] = $post->getDtCreate();
+            $postContent['update_time'] = $post->getDtUpdate();
+            $response[] = $postContent;
+        }
+
+        return $response;
+    }
+
+    /**
+     * 取得依Filter得到的資料
+     * @param Filter $filter
+     * @param Filter $messageFilter
+     * @return Response
+     */
+    private function generatePostsByFilter(Filter $filter, Filter $messageFilter)
+    {
+        if (( ! $filter->isValid()) or
+            ( ! $messageFilter->isValid())) {
+            $error = new ErrorArgument(ErrorArgument::ERROR_ARGUMENT_INVALID);
+            $statusCode = HttpStatusCode::STATUS_400_BAD_REQUEST;
+            return response()->json($error->convertToDisplayArray(), $statusCode);
+        }
+
+        $posts = PostModel::getList($filter, $messageFilter);
+        if ($posts == array()) {
+            $error = new ErrorArgument(ErrorArgument::ERROR_ARGUMENT_RESULT_NOT_FOUND);
+            $statusCode = HttpStatusCode::STATUS_400_BAD_REQUEST;
+            return response()->json($error->convertToDisplayArray(), $statusCode);
+        }
+
+        $totalAmount = count($posts);
+        $response = array();
+        $response['total_amount'] = $totalAmount;
+        $response['data'] = static::convertToPostsDisplayArray($posts);
+
+        $statusCode = HttpStatusCode::STATUS_200_OK;
+        return response()->json($response, $statusCode);
+    }
+
+    /**
+     * 取得部分討論主題
+     * URI: GET /api/posts
+     * @return Response
+     */
+    public function getPost()
+    {
+        $offset = (int)Input::get('offset', 0);
+        $limit = (int)Input::get('limit', 0);
+        $messageOffset = (int)Input::get('message_offset', 0);
+        $messageLimit = (int)Input::get('message_limit', 0);
+
+        // 設定過濾器
+        $filter = new Filter();
+        $filter->setOffset($offset);
+        $filter->setLimit($limit);
+        $messageFilter = new Filter();
+        $messageFilter->setOffset($messageOffset);
+        $messageFilter->setLimit($messageLimit);
+
+        return static::generatePostsByFilter($filter, $messageFilter);
+    }
+}
---
+++ tests/Unit/Http/Controller/api/PostControllerTest.php
@@ -0,0 +1,314 @@
+<?php namespace Tests;
+
+use App\Classes\Common\HttpStatusCode;
+use App\Classes\Errors\ErrorArgument;
+
+class PostControllerTest extends ApiTestCase
+{
+    /**
+     * 測試以API取得部分討論主題
+     * @return void
+     */
+    public function testGetPost()
+    {
+        // 設定input
+        $input = array('offset'   => 0
+                      , 'limit' => 10
+                      , 'message_offset' => 0
+                      , 'message_limit' => 10);
+
+        // 設定Header
+        $header = $this->header;
+
+        // 設定API路徑
+        $path = sprintf("/api/posts");
+
+        // 呼叫API
+        $response = $this->json('GET', $path, $input, $header);
+
+        // 確認回應的狀態碼是否正確
+        $this->assertEquals(HttpStatusCode::STATUS_200_OK, $response->getStatusCode());
+
+        // 回應的Json格式字串，轉成array後檢查內容是否符合預期
+        $contents = json_decode($response->getContent(), true);
+        $testContents = array();
+        $testContents['total_amount'] = 7;
+        $testContents['data'][0] =
+            array('id' => 1
+                , 'user_name'        => 'admin'
+                , 'user_sicker_type' => 1
+                , 'messages' => array('total_amount' => 4,
+                                      'data' => array(
+                                       array('user_name' => "admin",
+                                             'description' => "description01")
+                                       ,array('user_name' => "imily",
+                                              'description' => "description02")
+                                       ,array('user_name' => "Mary",
+                                              'description' => "description03")
+                                       ,array('user_name' => "imily",
+                                              'description' => "description05")))
+                , 'topic' => 'topic01'
+                , 'description' => 'description01'
+                , 'likes' => array(array('user_name' => 'admin'),
+                                   array('user_name' => 'imily'),
+                                   array('user_name' => 'Mary'))
+                , 'create_time' => "2011-11-11 00:00:00"
+                , 'update_time' => "2011-11-12 00:00:00");
+        $testContents['data'][1] =
+            array('id' => 2
+            , 'user_name'        => 'imily'
+            , 'user_sicker_type' => 3
+            , 'messages' => array('total_amount' => 3,
+                'data' => array(
+                    array('user_name' => "admin",
+                        'description' => "description01")
+                ,array('user_name' => "Mary",
+                        'description' => "description06")
+                ,array('user_name' => "admin",
+                        'description' => "description07")))
+            , 'topic' => 'topic02'
+            , 'description' => 'description02'
+            , 'likes' => array(array('user_name' => 'admin'),
+                array('user_name' => 'imily'),
+                array('user_name' => 'Mary'))
+            , 'create_time' => "2011-11-12 00:00:00"
+            , 'update_time' => "2011-11-13 00:00:00");
+        $testContents['data'][2] =
+            array('id' => 3
+            , 'user_name'        => 'Mary'
+            , 'user_sicker_type' => 1
+            , 'messages' => array('total_amount' => 3,
+                'data' => array(
+                    array('user_name' => "admin",
+                        'description' => "description01")
+                ,array('user_name' => "admin",
+                        'description' => "description04")
+                ,array('user_name' => "Mary",
+                        'description' => "description08")))
+            , 'topic' => 'topic03'
+            , 'description' => 'description03'
+            , 'likes' => array(array('user_name' => 'admin'),
+                array('user_name' => 'imily'),
+                array('user_name' => 'Mary'))
+            , 'create_time' => "2011-11-13 00:00:00"
+            , 'update_time' => "2011-11-14 00:00:00");
+        $testContents['data'][3] =
+            array('id' => 4
+            , 'user_name'        => 'imily'
+            , 'user_sicker_type' => 3
+            , 'messages' => array('total_amount' => 5,
+                'data' => array(
+                    array('user_name' => "admin",
+                        'description' => "description01")
+                ,array('user_name' => "imily",
+                        'description' => "description02")
+                ,array('user_name' => "Mary",
+                        'description' => "description03")
+                ,array('user_name' => "imily",
+                        'description' => "description05")
+                ,array('user_name' => "Mary",
+                        'description' => "description08")))
+            , 'topic' => 'topic04'
+            , 'description' => 'description04'
+            , 'likes' => array(array('user_name' => 'admin'),
+                array('user_name' => 'imily'),
+                array('user_name' => 'Mary'))
+            , 'create_time' => "2011-11-14 00:00:00"
+            , 'update_time' => "2011-11-15 00:00:00");
+        $testContents['data'][4] =
+            array('id' => 5
+            , 'user_name'        => 'admin'
+            , 'user_sicker_type' => 1
+            , 'messages' => array('total_amount' => 4,
+                'data' => array(
+                    array('user_name' => "admin",
+                        'description' => "description01")
+                ,array('user_name' => "imily",
+                        'description' => "description02")
+                ,array('user_name' => "Mary",
+                        'description' => "description03")
+                ,array('user_name' => "admin",
+                        'description' => "description04")))
+            , 'topic' => 'topic05'
+            , 'description' => 'description05'
+            , 'likes' => array(
+                array('user_name' => 'imily'),
+                array('user_name' => 'Mary'))
+            , 'create_time' => "2011-11-15 00:00:00"
+            , 'update_time' => "2011-11-16 00:00:00");
+        $testContents['data'][5] =
+            array('id' => 6
+            , 'user_name'        => 'Mary'
+            , 'user_sicker_type' => 1
+            , 'messages' => array('total_amount' => 3,
+                'data' => array(
+                    array('user_name' => "admin",
+                        'description' => "description01")
+                ,array('user_name' => "imily",
+                        'description' => "description05")
+                ,array('user_name' => "Mary",
+                        'description' => "description08")))
+            , 'topic' => 'topic06'
+            , 'description' => 'description06'
+            , 'likes' => array(
+                array('user_name' => 'admin'),
+                array('user_name' => 'imily'))
+            , 'create_time' => "2011-11-16 00:00:00"
+            , 'update_time' => "2011-11-17 00:00:00");
+        $testContents['data'][6] =
+            array('id' => 7
+            , 'user_name'        => 'imily'
+            , 'user_sicker_type' => 3
+            , 'messages' => array('total_amount' => 3,
+                'data' => array(
+                    array('user_name' => "imily",
+                        'description' => "description02")
+                ,array('user_name' => "imily",
+                        'description' => "description05")
+                ,array('user_name' => "Mary",
+                        'description' => "description06")))
+            , 'topic' => 'topic07'
+            , 'description' => 'description07'
+            , 'likes' => array(
+                array('user_name' => 'admin'),
+                array('user_name' => 'imily'))
+            , 'create_time' => "2011-11-17 00:00:00"
+            , 'update_time' => "2011-11-18 00:00:00");
+
+        $this->assertEquals($testContents, $contents);
+    }
+
+        /**
+     * 測試以API取得部分討論主題
+     * (offset設定為5，limit設定為2)
+     * @return void
+     */
+    public function testGetPostForOffset()
+    {
+        // 設定input
+        $input = array('offset'   => 5
+                     , 'limit' => 2
+                     , 'message_offset' => 0
+                     , 'message_limit' => 10);
+
+        // 設定Header
+        $header = $this->header;
+
+        // 設定API路徑
+        $path = sprintf("/api/posts");
+
+        // 呼叫API
+        $response = $this->json('GET', $path, $input, $header);
+
+        // 確認回應的狀態碼是否正確
+        $this->assertEquals(HttpStatusCode::STATUS_200_OK, $response->getStatusCode());
+
+        // 回應的Json格式字串，轉成array後檢查內容是否符合預期
+        $contents = json_decode($response->getContent(), true);
+        $testContents = array();
+        $testContents['total_amount'] = 2;
+
+        $testContents['data'][0] =
+            array('id' => 6
+            , 'user_name'        => 'Mary'
+            , 'user_sicker_type' => 1
+            , 'messages' => array('total_amount' => 3,
+                'data' => array(
+                    array('user_name' => "admin",
+                        'description' => "description01")
+                ,array('user_name' => "imily",
+                        'description' => "description05")
+                ,array('user_name' => "Mary",
+                        'description' => "description08")))
+            , 'topic' => 'topic06'
+            , 'description' => 'description06'
+            , 'likes' => array(
+                array('user_name' => 'admin'),
+                array('user_name' => 'imily'))
+            , 'create_time' => "2011-11-16 00:00:00"
+            , 'update_time' => "2011-11-17 00:00:00");
+        $testContents['data'][1] =
+            array('id' => 7
+            , 'user_name'        => 'imily'
+            , 'user_sicker_type' => 3
+            , 'messages' => array('total_amount' => 3,
+                'data' => array(
+                    array('user_name' => "imily",
+                        'description' => "description02")
+                ,array('user_name' => "imily",
+                        'description' => "description05")
+                ,array('user_name' => "Mary",
+                        'description' => "description06")))
+            , 'topic' => 'topic07'
+            , 'description' => 'description07'
+            , 'likes' => array(
+                array('user_name' => 'admin'),
+                array('user_name' => 'imily'))
+            , 'create_time' => "2011-11-17 00:00:00"
+            , 'update_time' => "2011-11-18 00:00:00");
+        $this->assertEquals($testContents, $contents);
+    }
+
+    /**
+     * 測試以API取得部分討論主題
+     * (Fail: offset設定無效參數)
+     * @return void
+     */
+    public function testGetPostForOffsetInvalid()
+    {
+        // 設定input
+        $input = array('offset'   => -1
+                     , 'limit' => 2
+                     , 'message_offset' => 0
+                     , 'message_limit' => 10);
+
+        // 設定Header
+        $header = $this->header;
+
+        // 設定API路徑
+        $path = sprintf("/api/posts");
+
+        // 呼叫API
+        $response = $this->json('GET', $path, $input, $header);
+
+        // 確認回應的狀態碼是否正確
+        $this->assertEquals(HttpStatusCode::STATUS_400_BAD_REQUEST, $response->getStatusCode());
+
+        // 回應的Json格式字串，轉成array後檢查內容是否符合預期。
+        $contents = json_decode($response->getContent(), true);
+        $error = new ErrorArgument(ErrorArgument::ERROR_ARGUMENT_INVALID);
+        $this->assertEquals($error->convertToDisplayArray(), $contents);
+    }
+
+    /**
+     * 測試以API取得部分討論主題
+     * (Fail: limit設定為0)
+     * @return void
+     */
+    public function testGetPostForLimitInvalid()
+    {
+        // 設定input
+        $input = array('offset'   => 0
+                     , 'limit' => ''
+                     , 'message_offset' => 0
+                     , 'message_limit' => 10);
+
+        // 設定Header
+        $header = $this->header;
+
+        // 設定API路徑
+        $path = sprintf("/api/posts");
+
+        // 呼叫API
+        $response = $this->json('GET', $path, $input, $header);
+
+        // 確認回應的狀態碼是否正確
+        $this->assertEquals(HttpStatusCode::STATUS_400_BAD_REQUEST, $response->getStatusCode());
+
+        // 回應的Json格式字串，轉成array後檢查內容是否符合預期。
+        $contents = json_decode($response->getContent(), true);
+        $error = new ErrorArgument(ErrorArgument::ERROR_ARGUMENT_INVALID);
+        $this->assertEquals($error->convertToDisplayArray(), $contents);
+    }
+
+}
